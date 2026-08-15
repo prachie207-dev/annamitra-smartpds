ea270ba2b49a77041267761d223be0a897eb14d5
prachie207-dev <prachie207@gmail.com>
2026-08-15 18:37:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,15 +18,13 @@ const otpStore = new Map();
 app.use(cors());
 app.use(express.json());
 
-// Serve static files and index.html from the current directory root
-const rootPath = path.join(__dirname);
+// Correctly point to the main project root folder one level up
+const rootPath = path.join(__dirname, '..');
 app.use(express.static(rootPath));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(rootPath, 'index.html'));
-});
+app.use('/css', express.static(path.join(rootPath, 'css')));
 app.use('/js', express.static(path.join(rootPath, 'js')));
 app.use('/images', express.static(path.join(rootPath, 'images')));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(rootPath, 'index.html'));
 });
